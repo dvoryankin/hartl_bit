01760546188833f3efd5bd36f4d7956fb7107ff6
dvoryankin <dvoryankindm@gmail.com>
2019-02-25 13:52:30 +0500

    add dynamic title to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,26 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
-#class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+# class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    # get static_pages_home_url
-    get :home
+    get static_pages_home_url
+    # get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    # get static_pages_help_url
-    get :help
+    get static_pages_help_url
+    # get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    # get static_pages_about_url
-    get :about
+    get static_pages_about_url
+    # get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
