83568e3dd1665c8ec12fffe0b3aa6fb7ef49a009
dvoryankin <dvoryankindm@gmail.com>
2019-02-25 11:37:18 +0500

    add about page and test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    it "should have the content 'root'" do
+      visit '/'
+      expect(page).to have_content('root')
+    end
+  end
 
+  describe "static_pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -14,9 +20,9 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the content 'root'" do
-      visit '/'
-      expect(page).to have_content('root')
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
     end
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,23 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionController::TestCase
+#class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get static_pages_home_url
+    # get static_pages_home_url
+    get :home
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    # get static_pages_help_url
+    get :help
     assert_response :success
   end
 
   test "should get about" do
+    # get static_pages_about_url
     get :about
     assert_response :success
   end
-
 end
