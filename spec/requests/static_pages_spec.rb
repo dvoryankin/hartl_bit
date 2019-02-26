bb259439694573c5753f4d8a1bb7c999b77e94cb
dvoryankin <dvoryankindm@gmail.com>
2019-02-26 15:54:00 +0500

    fix rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,16 +5,11 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'root'" do
       visit '/'
-      expect(page).to have_content('root')
+      expect(page).to have_content('Sample App')
     end
   end
 
   describe "static_pages" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -23,6 +18,11 @@ describe "Static pages" do
     it "should have the content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
     end
   end
 end
