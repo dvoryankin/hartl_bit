16653b3dd27ef40a37e7de3c936d197b73ae8de7
dvoryankin <dvoryankindm@gmail.com>
2019-03-11 00:34:03 +0500

    create signup form, gravatar helper, dev inform, user_params test, user forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # binding.pry
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # get 'static_pages/contact'
   get 'contact' => 'static_pages#contact'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'welcome#index'
   # root 'application#hello'
