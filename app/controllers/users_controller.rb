098f29019e353f39438172f21eff9429b4b10bb9
dvoryankin <dvoryankindm@gmail.com>
2019-03-11 17:44:59 +0500

    add user create action, user_params, errors messages and css for it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # binding.pry
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      p 'success'
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -13,7 +21,9 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
