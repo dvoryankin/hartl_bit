f08530aec838a77127568c63eb26e624ce277f8c
dvoryankin <dvoryankindm@gmail.com>
2019-03-14 18:12:22 +0500

    add cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,22 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Возвращает текущего вошедшего пользователя (если есть).
+  def rememer(user)
+    cookies.permanent.signed[user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Возвращает пользователя, соответствующего токену в cookie.
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Возвращает true, если пользователь зарегистрирован, иначе возвращает false.
