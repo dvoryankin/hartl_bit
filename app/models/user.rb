f9092a87d223fe0660cc4279de205828ebeef2a9
dvoryankin <dvoryankindm@gmail.com>
2019-03-19 16:35:15 +0500

    add user forget when logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ApplicationRecord
 
   # Возвращает true, если указанный токен соответствует дайджесту.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
