0715cb3b21cb12b0ba395dcd537d48deebc8481d
dvoryankin <dvoryankindm@gmail.com>
2019-03-21 13:18:18 +0500

    add remember me ticket, finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,28 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 
+  # Return true if test user logged in
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Test user log in
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, params: { session: { email:    user.email,
+                                  password: password,
+                                  remember_me: remember_me } }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    #Return true inside integration test
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
