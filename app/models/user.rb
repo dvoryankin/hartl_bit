a6f05266678136087f030064b961f5427496ad1f
dvoryankin <dvoryankindm@gmail.com>
2019-03-27 11:49:08 +0500

    add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,6 +65,11 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Возвращает true, если время для сброса пароля истекло.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     # Преобразует адрес электронной почты в нижний регистр.
