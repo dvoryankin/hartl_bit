8acf7a5641df21e878d4d961df7f558f39ba452f
dvoryankin <dvoryankindm@gmail.com>
2019-03-27 11:09:03 +0500

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
 
   root to: 'static_pages#home'
 
