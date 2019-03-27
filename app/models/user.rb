0d9d40f51337ccc9bce5236bc79dfc8d0904d1dc
dvoryankin <dvoryankindm@gmail.com>
2019-03-27 17:53:50 +0500

    add proto feed-line to microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,6 +71,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where('user_id = ?', id)
+  end
+
   private
 
     # Преобразует адрес электронной почты в нижний регистр.
