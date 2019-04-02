f5ad7e5f41eb3f0246679b4a9f9f9b794bc9fd08
dvoryankin <dvoryankindm@gmail.com>
2019-04-02 17:22:05 +0500

    add main methods, tests and relations for following feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -73,6 +83,24 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  # Выполняет подписку на сообщения пользователя.
+  def follow(other_user)
+    following << other_user
+    # active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Отменяет подписку на сообщения пользователя.
+  def unfollow(other_user)
+    following.delete(other_user)
+    # active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Возвращает true, если текущий пользователь читает
+  #другого пользователя.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
